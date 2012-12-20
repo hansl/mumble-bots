40cdd9a093bf5bb50a83f504fe5c9c9c7edd0c80
Hans Larsen <hansl@google.com>
2012-12-20 10:27:28 -0800

    First test using a local server, and normally finished the first pass of connection.

--- mumble/server.py
+++ mumble/server.py
@@ -2,23 +2,24 @@
 
 import logging
 import socket
+import ssl
 
 # Keep a server's information in a nice tidy place.
 # This is hardly more than that.
-class server(object):
+class Server(object):
   def __init__(self, hostname = '', port = 64738):
     self.hostname = hostname
     self.port = port
     self.logger = logging.getLogger(self.__class__.__name__)
 
   def connect(self):
-    socket = ssl.wrap_socket(socket.socket(type = socket.SOCK_STREAM),
-                                           ssl_version = ssl.PROTOCOL_TLSv1)
-    socket.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, 1)
+    sc = ssl.wrap_socket(socket.socket(type = socket.SOCK_STREAM),
+                                       ssl_version = ssl.PROTOCOL_TLSv1)
+    sc.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, 1)
     try:
-        socket.connect((self.hostname, self.port))
+        sc.connect((self.hostname, self.port))
     except socket.error as e:
         self.logger.error("Couldn't connect to server")
         raise e
 
-    return socket
+    return sc
