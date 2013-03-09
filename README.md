91928f7d7f9589ffaf81421b82ae6935c551ecb6
Hans Larsen <hans@hansl.ca>
2013-03-09 12:02:46 -0800

    Added some stuff to README (still WIP). Added channel commands support.

--- README.md
+++ README.md
@@ -3,4 +3,21 @@ mumble-bots
 
 A list of bots for Mumble Servers.
 
+# Commanding bots
+The API is simple; use slash commands in messages to bots.
 
+List of supported commands by default:
+1. /add
+2. 
+
+# Included Bots
+## AFKMove
+A bot
+
+## Echo
+A test bot that reply back a message to the sender. This does not support any bang or slash commands.
+
+## Eve
+A bot that eavesdrop on a channel and replays it to another channel, potentially with a delay.
+
+## Dice
--- mumble/command_bot.py
+++ mumble/command_bot.py
@@ -26,22 +26,33 @@ from bot import Bot
 LOGGER = logging.getLogger(__name__)
 
 class CommandBot(Bot):
-  DEFAULT_PREFIXES = {
+  TO_DEFAULT_PREFIXES = {
     '!': 'on_bang',
   }
+  CHAN_DEFAULT_PREFIXES = {}
 
-  def __init__(self, command_prefixes = CommandBot.DEFAULT_PREFIXES,
+  def __init__(self, command_prefixes = CommandBot.TO.DEFAULT_PREFIXES,
+                     channel_command_prefixes =
+                         CommandBot.CHAN_DEFAULT_PREFIXES,
                      name = "CommandBot by HansL"):
     Bot.__init__(self, name = name)
     self.__prefixes = command_prefixes
+    self.__channel_prefixes = channel_command_prefixes
 
   def on_text_message(self, from, user_ids, channel_ids, tree_ids, message):
     # Only answer to direct messages, not channel messages.
-    if self.state.session_id in user_ids:
+    if self.state.user.id in user_ids:
       cmd = shlex.split(m.group(1))
       for p in self.__prefixes.keys():
         if cmd[0].startswith(p):
-          getattr(self, __prefixes[p])(self, from = from, *cmd)
+          func = getattr(self, __prefixes[p])
+          func(self, from = from, *cmd)
+          return
+    if self.state.channel.id in channel_ids:
+      for p in self.__channel_prefixes.keys():
+        if cmd[0].startswith(p):
+          func = getattr(self, self.__channel_prefixes[p])
+          func(self, from = from, *cmd)
           return
     Bot.on_text_message(self, from, user_ids, channel_ids, tree_ids, message)
 
